2153d228ef57eb9dc9a926343f58d460415cafd6
Steven <stevenbuglion1@gmail.com>
2024-09-08 02:06:01 -0400

    Created Dockerfile and Script to locally test Docker container

---
+++ src/FabricCli/fabric.sh
@@ -0,0 +1,23 @@
+﻿#!/bin/bash
+
+# Ensure that at least one argument is passed
+if [ "$#" -lt 1 ]; then
+    echo "Usage: $0 <command> [arguments...]"
+    exit 1
+fi
+
+# Get environment variables from the system
+AZURE_TENANT_ID=${AZURE_TENANT_ID}
+AZURE_CLIENT_ID=${AZURE_CLIENT_ID}
+AZURE_CLIENT_SECRET=${AZURE_CLIENT_SECRET}
+
+
+# Run Docker container with environment variables
+docker run --rm \
+  -e AZURE_TENANT_ID="$AZURE_TENANT_ID" \
+  -e AZURE_CLIENT_ID="$AZURE_CLIENT_ID" \
+  -e AZURE_CLIENT_SECRET="$AZURE_CLIENT_SECRET" \
+  sbuglione/fabric:latest "$@"
+
+
+
